23ba54b77c56abdb3a97ddda8e06bbeae8be5340
Stringmander <nrusso@protonmail.com>
2020-10-20 10:45:28 -0400

    clean up LoginController & create router singleton

---
+++ src/AppRouter.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+export class AppRouter {
+  private static routerInstance: express.Router;
+
+  static get instance(): express.Router {
+    if (!AppRouter.routerInstance) {
+      AppRouter.routerInstance = express.Router();
+    }
+
+    return AppRouter.routerInstance;
+  }
+}
