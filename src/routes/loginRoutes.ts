d53f2e88aaaba220ba0154d00196a93fe4551f49
Stringmander <nrusso@protonmail.com>
2020-10-20 09:47:51 -0400

    install reflect-metadata & begin refactor

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,5 +1,3 @@
-import { Router, Request, Response, NextFunction } from 'express';
-
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
 }
@@ -15,22 +13,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction): void {
 }
 
 const router = Router();
-
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-      <div>
-        <label>Email</label>
-        <input name="email" />
-      </div>
-      <div>
-        <label>Password</label>
-        <input name="password" type="password" />
-      </div>
-      <button>Submit</button>
-    </form>
-  `);
-});
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
